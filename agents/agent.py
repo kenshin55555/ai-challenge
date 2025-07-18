2c88db50c93b2b2fb0fc6a5fa2ca702cf49fcd33
kenshin55555 <guzmanmario5000@gmail.com>
2025-07-18 15:22:56 -0600

    Updated function logic to include agent call as a tool, also split promt logic for better readability

--- agents/agent.py
+++ agents/agent.py
@@ -1,7 +1,10 @@
 from google.adk.agents import Agent
-from tools.tools import fetch_greeting
-from google.adk.tools import google_search
+from google.adk.tools.agent_tool import AgentTool
 from config.settings import MODEL_GEMINI
+from .prompts.root_prompt import ROOT_PROMPT
+from .subagents.a.agent import create_stock_agent
+
+stock_agent = create_stock_agent()
 
 def create_greeting_agent():
     """
@@ -11,15 +14,8 @@ def create_greeting_agent():
     root_agent = Agent(
         name="greeting_agent", # A unique name for this specific agent.
         model=MODEL_GEMINI,    # Specifies the Gemini model to power this agent's language understanding and generation.
-        description="An agent that greets the user based on their name, hobbies, and interests.", # A brief, human-readable description of the agent's role.
-        instruction=""" # The core instructions that dictate the agent's behavior and how it should use its tools.
-        You are a helpful assistant that will answer the user's questions using google search.
-        
-        In case that the question involves weather, you will provide:
-        - Celsius degrees as the main value, farenheit degress as secondary
-        - Weather for the next 3 hours with Celsius degrees as the main value, farenheit degress as secondary 
-        - A random fact about the location provided                 
-        """,
-        tools=[google_search], # Registers the 'fetch_greeting' function as a callable tool for this agent.
+        description="An agent that orchestrates access to stock information", # A brief, human-readable description of the agent's role.
+        instruction=ROOT_PROMPT,
+        tools=[AgentTool(agent=stock_agent)], 
     )
     return root_agent
